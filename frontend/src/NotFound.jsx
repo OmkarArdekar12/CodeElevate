98d199dfc7ae37dcc18d6a5a5f92ed0d61edeeab
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-06-09 19:11:51 +0530

    Handling Errors

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import { BsEmojiFrownFill } from "react-icons/bs";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="text-white m-9">
+      <h1 className="text-5xl text-red-500 my-6">Oops! Error 404</h1>
+      <p className="ml-2 text-2xl">
+        Page Not Found! <BsEmojiFrownFill className="text-red-500 inline" />
+      </p>
+      <p className="ml-2 mb-6 text-xl italic">
+        Sorry, Page you are trying to access does not exist!
+      </p>
+      <Link to="/" className="text-xl text-blue-500 hover:underline">
+        Click Here! to Go Back To Home
+      </Link>
     </div>
   );
 }
