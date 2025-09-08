0dcb88b16da92f62308fb12cc876cf3fbcac0f24
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-08 23:07:05 +0530

    Profile Schema

--- backend/schemas/profileSchema.js
+++ backend/schemas/profileSchema.js
@@ -2,4 +2,59 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const profileSchema = Schema({});
+const profileSchema = Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  backgroundBanner: {
+    type: String,
+    default: "",
+  },
+  headLine: {
+    type: String,
+    default: "",
+  },
+  tags: [String],
+  about: {
+    type: String,
+    default: "",
+  },
+
+  developmentProfiles: {
+    github: String,
+    gitlab: String,
+    portfolio: String,
+  },
+  competitiveProfile: {
+    leetCode: String,
+    codeforces: String,
+    hackerrank: String,
+    geekforgeeks: String,
+    codeshef: String,
+  },
+  education: {
+    degree: String,
+    cgpa: Number,
+    institution: String,
+  },
+
+  followers: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  following: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+export default profileSchema;
