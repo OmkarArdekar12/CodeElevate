547bbb413345b140b313011c56e0adff9d89d49f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-08 23:02:21 +0530

    User Schema

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -16,57 +16,10 @@ const userSchema = new Schema(
 
     isMfaActive: {
       type: Boolean,
+      default: false,
     },
     twoFactorSecret: {
       type: String,
-    },
-
-    email: {
-      type: String,
-    },
-    profilePicture: {
-      type: String,
-      default: "",
-    },
-    backgroundBanner: {
-      type: String,
-      default: "",
-    },
-    headLine: {
-      type: String,
-      default: "",
-    },
-    tags: [String],
-    about: {
-      type: String,
-      default: "",
-    },
-
-    developmentProfiles: {
-      github: String,
-      gitlab: String,
-      portfolio: String,
-    },
-    competitiveProfile: {
-      leetCode: String,
-      codeforces: String,
-      hackerrank: String,
-      geekforgeeks: String,
-      codeshef: String,
-    },
-    education: {
-      degree: String,
-      cgpa: Number,
-      institution: String,
-    },
-
-    followers: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    following: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
     },
   },
   {
