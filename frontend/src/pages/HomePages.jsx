f2d962160edf7e7da8c67890f9834643a7ec13b9
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-12 17:05:31 +0530

    updated

--- frontend/src/pages/HomePages.jsx
+++ frontend/src/pages/HomePages.jsx
@@ -13,3 +13,31 @@ function HomePage() {
 }
 
 export default HomePage;
+
+// import "./App.css";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from "../components/Navbar.jsx";
+// import Footer from "../components/Footer.jsx";
+// import HomePage from "../home/Home.jsx";
+// import PostPage from "../posts/PostPage.jsx";
+// import RankingPage from "../rankings/RankingPage.jsx";
+// import AboutPage from "../about/AboutPage.jsx";
+// import NotFound from "../components/NotFound.jsx";
+
+// function HomePage() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/posts" element={<PostPage />} />
+//         <Route path="/rankings" element={<RankingPage />} />
+//         <Route path="/about" element={<AboutPage />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default HomePage;
