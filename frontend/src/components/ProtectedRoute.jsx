2a7a184ff96000df78d66550e3f31455520e7020
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-13 18:41:11 +0530

    Setting up two-factor-authentication setup

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,7 +3,15 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSession } from "../context/SessionContext";
 
 const ProtectedRoute = () => {
-  const { isLoggedIn } = useSession();
+  const { isLoggedIn, loading } = useSession();
+  console.log("The LoggedIn: ", isLoggedIn);
+  if (loading) {
+    return (
+      <div className="w-[100%] h-[100%] text-white bg-slate-900 flex flex-col justify-center items-center px-5 py-9">
+        Loading...
+      </div>
+    );
+  }
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
 };
 
--- frontend/src/pages/Setup2FA.jsx
+++ frontend/src/pages/Setup2FA.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import TwoFASetup from "../components/TwoFASetup.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Setup2FA = () => {
-  return <TwoFASetup />;
+  const navigate = useNavigate();
+  const handleSetupComplete = () => {
+    navigate("/verify-2fa");
+  };
+  return (
+    <div className="w-[100%] h-[100%] text-white bg-slate-900 flex flex-col justify-center items-center px-5 py-9">
+      <div className="logo-text-container inline-flex justify-center items-center mb-1">
+        <img src="/images/logo.png" alt="CodeElevate" className="logo h-10" />
+        <h1 className="logo-text ml-1 text-white text-3xl">CodeElevate</h1>
+      </div>
+      <TwoFASetup onSetupComplete={handleSetupComplete} />
+    </div>
+  );
 };
 
 export default Setup2FA;
