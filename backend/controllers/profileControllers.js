cebeb23cd0037b5e1bd321b0aec7eb63ba1cb21f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-16 20:20:40 +0530

    Working on Profiles

---
+++ backend/controllers/profileControllers.js
@@ -0,0 +1,91 @@
+import express from "express";
+import auth from "../middlewares/auth.js";
+import Profile from "../models/profile.js";
+
+export const getAllProfiles = async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", "username");
+    res.status(200).json(profiles);
+  } catch (err) {
+    res.status(500).json({ message: "Error in fetching profiles", error: err });
+  }
+};
+
+export const showProfile = async (req, res) => {
+  try {
+    const id = req.params.userId;
+    const profile = await Profile.findOne({ user: id }).populate(
+      "user",
+      "username"
+    );
+    if (!profile) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+    res.status(200).json(profile);
+  } catch (err) {
+    res.status(500).json({ message: "Error in fetching profile", error: err });
+  }
+};
+
+export const createProfile = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    let profile = await Profile.findOne({ user: userId });
+    if (profile) {
+      profile = Object.assign(profile, req.body);
+      await profile.save();
+      return res.status(200).json(profile);
+    }
+
+    profile = new Profile({ user: userId, ...req.body });
+    await profile.save();
+    res.status(200).json(profile);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error in creating/updating profile", error: err });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const clientUserId = req.params.userId;
+    const ownerUserId = req.user._id.toString();
+
+    if (clientUserId !== ownerUserId) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to update this profile" });
+    }
+
+    const profile = await Profile.findOne({ user: clientUserId });
+
+    if (!profile) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+
+    Object.assign(profile, req.body);
+    await profile.save();
+    res.status(200).json(profile);
+  } catch (err) {
+    res.status(500).json({ message: "Error in updating profile", error: err });
+  }
+};
+
+export const destroyProfile = async (req, res) => {
+  try {
+    const clientUserId = req.params.userId;
+    const ownerUserId = req.user._id.toString();
+
+    if (clientUserId !== ownerUserId) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to delete this profile" });
+    }
+
+    await Profile.findOneAndDelete({ user: req.params.userid });
+    res.json({ message: "Profile deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting profile" });
+  }
+};
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,100 +1,29 @@
 import express from "express";
 import auth from "../middlewares/auth.js";
 import Profile from "../models/profile.js";
+import {
+  getAllProfiles,
+  showProfile,
+  createProfile,
+  updateProfile,
+  destroyProfile,
+} from "../controllers/profileControllers.js";
 
 const router = express.Router();
 
 //All Profiles Route
-router.get("/", async (req, res) => {
-  try {
-    const profiles = await Profile.find().populate("user", "username");
-    res.status(200).json(profiles);
-  } catch (err) {
-    res.status(500).json({ message: "Error in fetching profiles", error: err });
-  }
-});
+router.get("/", getAllProfiles);
 
 //Show Profile Route
-router.get("/:userId", async (req, res) => {
-  try {
-    const id = req.params.userId;
-    const profile = await Profile.findOne({ user: id }).populate(
-      "user",
-      "username"
-    );
-    if (!profile) {
-      return res.status(404).json({ message: "Profile not found" });
-    }
-    res.status(200).json(profile);
-  } catch (err) {
-    res.status(500).json({ message: "Error in fetching profile", error: err });
-  }
-});
+router.get("/:userId", showProfile);
 
 // Create Profile Route
-router.post("/", auth, async (req, res) => {
-  try {
-    const userId = req.user._id;
-    let profile = await Profile.findOne({ user: userId });
-    if (profile) {
-      profile = Object.assign(profile, req.body);
-      await profile.save();
-      return res.status(200).json(profile);
-    }
-
-    profile = new Profile({ user: userId, ...req.body });
-    await profile.save();
-    res.status(200).json(profile);
-  } catch (err) {
-    res
-      .status(500)
-      .json({ message: "Error in creating/updating profile", error: err });
-  }
-});
+router.post("/", auth, createProfile);
 
 // Update Profile Route
-router.put("/:userId", auth, async (req, res) => {
-  try {
-    const clientUserId = req.params.userId;
-    const ownerUserId = req.user._id.toString();
-
-    if (clientUserId !== ownerUserId) {
-      return res
-        .status(403)
-        .json({ message: "Unauthorized to update this profile" });
-    }
-
-    const profile = await Profile.findOne({ user: clientUserId });
-
-    if (!profile) {
-      return res.status(404).json({ message: "Profile not found" });
-    }
-
-    Object.assign(profile, req.body);
-    await profile.save();
-    res.status(200).json(profile);
-  } catch (err) {
-    res.status(500).json({ message: "Error in updating profile", error: err });
-  }
-});
+router.put("/:userId", auth, updateProfile);
 
 //Delete Profile Route
-router.delete("/:userId", auth, async (req, res) => {
-  try {
-    const clientUserId = req.params.userId;
-    const ownerUserId = req.user._id.toString();
-
-    if (clientUserId !== ownerUserId) {
-      return res
-        .status(403)
-        .json({ message: "Unauthorized to delete this profile" });
-    }
-
-    await Profile.findOneAndDelete({ user: req.params.userid });
-    res.json({ message: "Profile deleted" });
-  } catch (err) {
-    res.status(500).json({ message: "Error deleting profile" });
-  }
-});
+router.delete("/:userId", auth, destroyProfile);
 
 export default router;
