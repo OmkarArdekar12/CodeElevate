813d3cf768383af49c720e1307c087533832a847
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-16 20:12:52 +0530

    Working on Profiles

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -31,6 +31,70 @@ router.get("/:userId", async (req, res) => {
   }
 });
 
-//
+// Create Profile Route
+router.post("/", auth, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    let profile = await Profile.findOne({ user: userId });
+    if (profile) {
+      profile = Object.assign(profile, req.body);
+      await profile.save();
+      return res.status(200).json(profile);
+    }
+
+    profile = new Profile({ user: userId, ...req.body });
+    await profile.save();
+    res.status(200).json(profile);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error in creating/updating profile", error: err });
+  }
+});
+
+// Update Profile Route
+router.put("/:userId", auth, async (req, res) => {
+  try {
+    const clientUserId = req.params.userId;
+    const ownerUserId = req.user._id.toString();
+
+    if (clientUserId !== ownerUserId) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to update this profile" });
+    }
+
+    const profile = await Profile.findOne({ user: clientUserId });
+
+    if (!profile) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+
+    Object.assign(profile, req.body);
+    await profile.save();
+    res.status(200).json(profile);
+  } catch (err) {
+    res.status(500).json({ message: "Error in updating profile", error: err });
+  }
+});
+
+//Delete Profile Route
+router.delete("/:userId", auth, async (req, res) => {
+  try {
+    const clientUserId = req.params.userId;
+    const ownerUserId = req.user._id.toString();
+
+    if (clientUserId !== ownerUserId) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to delete this profile" });
+    }
+
+    await Profile.findOneAndDelete({ user: req.params.userid });
+    res.json({ message: "Profile deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting profile" });
+  }
+});
 
 export default router;
