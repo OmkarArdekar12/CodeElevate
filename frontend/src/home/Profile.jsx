59bf84c642328e83cd97899ad8cc1fac8abba78b
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-16 22:00:53 +0530

    UI Designing

--- frontend/src/home/Profile.jsx
+++ frontend/src/home/Profile.jsx
@@ -7,35 +7,35 @@ export default function Profile({ user }) {
   const tags = ["Coder", "Learner", "Speed"];
   return (
     <div className="Profile flex flex-wrap w-[100%] text-xl items-center justify-between bg-gradient-to-r from-gray-950 to-blue-900 rounded-xl px-10 py-2 my-4 shadow-sm shadow-slate-500/50">
-      <div className="flex flex-wrap items-center justify-evenly">
+      <div className="profile-image flex flex-wrap items-center justify-evenly">
         <img src={userImage} alt={username} className="w-[5rem]" />
         <div className="flex flex-col justify-center">
-          <p className="my-1 mx-5 mr-10">{username}</p>
-          {headLine && <p className="my-1 mx-5 mr-10 text-xs">{headLine}</p>}
+          <p className="mx-2">{username}</p>
+          {headLine && <p className="mx-2 text-xs">{headLine}</p>}
         </div>
       </div>
-      <div className="flex flex-wrap items-center">
-        <div className="flex items-center">
-          <p className="my-1 mx-2 bg-gray-600 p-2 px-4 rounded-2xl italic text-sm">
+      <div className="profile-description flex flex-wrap items-center">
+        <div className="m-1 flex flex-wrap items-center justify-center">
+          <p className="m-1 bg-gray-600 py-2 px-4 rounded-2xl italic text-sm">
             {role}
           </p>
-          <p className="my-1 mx-2 bg-gray-600 p-2 px-4 rounded-2xl italic text-sm">
+          <p className="m-1 bg-gray-600 py-2 px-4 rounded-2xl italic text-sm">
             {domain}
           </p>
         </div>
         {tags && (
-          <div className="flex items-center">
-            {tags.map((tag, index) => {
+          <div className="m-1 flex flex-wrap flex-row justify-center">
+            {tags.map((tag, index) => (
               <p
                 key={index}
-                className="my-1 mx-2 bg-gray-600 p-2 px-4 rounded-2xl italic text-sm"
+                className="m-[1px] bg-gray-700 px-4 py-2 rounded-3xl text-sm"
               >
                 {tag}
-              </p>;
-            })}
+              </p>
+            ))}
           </div>
         )}
-        <button className="my-1 ml-8 bg-blue-700 px-6 py-2 rounded-full hover:bg-green-700 hover:border-green-700">
+        <button className="m-2 bg-blue-700 px-6 py-2 rounded-full hover:bg-green-700 hover:border-green-700">
           Visit
         </button>
       </div>
