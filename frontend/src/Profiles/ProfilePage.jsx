1f97af9116f04dcff18618ca29ac5a10f918c384
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-17 18:52:02 +0530

    Working on Competitive Programming stats

--- frontend/src/Profiles/ProfilePage.jsx
+++ frontend/src/Profiles/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { FaCheckCircle } from "react-icons/fa";
 import {
   SiLeetcode,
   SiCodeforces,
@@ -10,6 +11,7 @@ import {
   SiTopcoder,
 } from "react-icons/si";
 import { GiHorseHead } from "react-icons/gi";
+import { FiExternalLink } from "react-icons/fi";
 
 const ProfilePage = () => {
   const tags = ["Coder", "Dominator", "TeamPlayer"];
@@ -47,15 +49,18 @@ const ProfilePage = () => {
           <button className="bg-blue-500 py-2 px-4 m-1 rounded-md hover:text-white hover:border-1 hover-text-border hover:bg-gradient-to-r hover:from-blue-500 hover:via-blue-600 hover:to-blue-700">
             Connect
           </button>
+          <button className="bg-blue-500 py-2 px-4 m-1 rounded-md hover:text-white hover:border-1 hover-text-border hover:bg-gradient-to-r hover:from-blue-500 hover:via-blue-600 hover:to-blue-700">
+            Message
+          </button>
         </div>
         {/* about */}
-        <hr className="w-full text-gray-600 mt-10 mb-8" />
+        <hr className="w-full text-gray-600 mt-25 mb-10" />
         <div className="w-full flex flex-col justify-center p-4">
           <h2 className="text-3xl mb-3">About</h2>
           <h3 className="text-lg">Here the about section</h3>
         </div>
         {/* domain & role */}
-        <hr className="w-full text-gray-600 my-6" />
+        <hr className="w-full text-gray-600 my-10" />
         <div className="w-full flex flex-col justify-center p-4">
           <h2 className="text-3xl mb-3">Domain & Role</h2>
           <div className="w-full flex items-center flex-wrap">
@@ -76,7 +81,7 @@ const ProfilePage = () => {
           </div>
         </div>
         {/* Tags */}
-        <hr className="w-full text-gray-600 my-6" />
+        <hr className="w-full text-gray-600 my-10" />
         <div className="w-full flex flex-col justify-center p-4">
           <h2 className="text-3xl mb-3">Tags</h2>
           <div className="w-full flex items-center flex-wrap">
@@ -91,22 +96,106 @@ const ProfilePage = () => {
           </div>
         </div>
         {/* CP */}
-        <hr className="w-full text-gray-600 my-6" />
+        <hr className="w-full text-gray-600 my-10" />
         <div className="w-full flex flex-col justify-center p-4">
           <h2 className="text-3xl mb-1">
             Competitive Programming / Data Structures & Algorithms
           </h2>
-          <div>
-            <div>LeetCode</div>
-            <div>Codeforces</div>
-            <div>AtCoder</div>
-            <div>CodeChef</div>
-            <div>GeeksForGeeks</div>
-            <div>HackerRank</div>
+          <div className="mt-4 p-4 md:px-25">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <div className="flex items-center space-x-2 flex-wrap">
+                <FaCheckCircle className="text-blue-500 w-5 h-5 inline" />
+                <div className="flex items-center space-x-1 flex-wrap group">
+                  <SiLeetcode className="text-[#FFA116] w-9 h-9" />
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="text-2xl hover-text-border hover:underline hover:decoration-[#FFA116] hover:decoration-2"
+                  >
+                    LeetCode
+                  </a>
+                  <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                </div>
+              </div>
+              <div className="flex items-center space-x-2 flex-wrap">
+                <FaCheckCircle className="text-blue-500 w-5 h-5" />
+                <div className="flex items-center space-x-1 flex-wrap group">
+                  <SiCodeforces className="text-[#1F8ACB] w-9 h-9" />
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="text-2xl hover-text-border hover:underline hover:decoration-[#1F8ACB] hover:decoration-2"
+                  >
+                    Codeforces
+                  </a>
+                  <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                </div>
+              </div>
+              <div className="flex items-center space-x-2 flex-wrap">
+                <FaCheckCircle className="text-blue-500 w-5 h-5" />
+                <div className="flex items-center space-x-1 flex-wrap group">
+                  <GiHorseHead className="text-gray-700 w-9 h-9" />
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="text-2xl hover-text-border hover:underline hover:decoration-gray-700 hover:decoration-2"
+                  >
+                    AtCoder
+                  </a>
+                  <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                </div>
+              </div>
+              <div className="flex items-center space-x-2 flex-wrap">
+                <FaCheckCircle className="text-blue-500 w-5 h-5" />
+                <div className="flex items-center space-x-1 flex-wrap group">
+                  <SiCodechef className="text-[#5B4638] w-9 h-9" />
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="text-2xl hover-text-border hover:underline hover:decoration-[#5B4638] hover:decoration-2"
+                  >
+                    CodeChef
+                  </a>
+                  <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                </div>
+              </div>
+              <div className="flex items-center space-x-2 flex-wrap">
+                <FaCheckCircle className="text-blue-500 w-5 h-5" />
+                <div className="flex items-center space-x-1 flex-wrap group">
+                  <SiGeeksforgeeks className="text-[#2F8D46] w-9 h-9" />
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="text-2xl hover-text-border hover:underline hover:decoration-[#2F8D46] hover:decoration-2"
+                  >
+                    GeeksforGeeks
+                  </a>
+                  <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                </div>
+              </div>
+              <div className="flex items-center space-x-2 flex-wrap">
+                <FaCheckCircle className="text-blue-500 w-5 h-5" />
+                <div className="flex items-center space-x-1 flex-wrap group">
+                  <SiHackerrank className="text-[#2EC866] w-9 h-9" />
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="text-2xl hover-text-border hover:underline hover:decoration-[#2EC866] hover:decoration-2"
+                  >
+                    HackerRank
+                  </a>
+                  <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        {/* Devs */}
+        <hr className="w-full text-gray-600 my-10" />
         <div>DevelopmentProfile</div>
+        {/* Edu */}
         <div>Education</div>
+        {/* Posts */}
         <div>Posts</div>
       </div>
     </div>
