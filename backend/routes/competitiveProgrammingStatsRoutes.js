f41c374240d33b15cac5999d55287c796278c7ad
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-18 11:41:11 +0530

    Competitive Programming Stats

--- backend/routes/competitiveProgrammingStatsRoutes.js
+++ backend/routes/competitiveProgrammingStatsRoutes.js
@@ -4,15 +4,42 @@ import axios from "axios";
 const router = express.Router();
 
 //Codeforces Stats Route/API
-router.get("/api/codeforces/:username", async (req, res) => {
+router.get("/codeforces/:username", async (req, res) => {
   const { username } = req.params;
   try {
-    const response = await axios.get(
+    const userInfo = await axios.get(
       `https://codeforces.com/api/user.info?handles=${username}`
     );
-    res.json(response);
+
+    const user = userInfo.data.result[0];
+
+    const userStatus = await axios.get(
+      `https://codeforces.com/api/user.status?handle=${username}`
+    );
+
+    const submissions = userStatus.data.result;
+
+    const problemSolvedSet = new Set();
+    submissions.forEach((sub) => {
+      if (sub.verdict == "OK") {
+        problemSolvedSet.add(`${sub.problem.contestId}-${sub.problem.index}`);
+      }
+    });
+
+    const codeforcesData = {
+      rating: user.rating || 0,
+      contribution: user.contribution || 0,
+      rank: user.rank || "unrated",
+      maxRating: user.maxRating || 0,
+      maxRank: user.maxRank || "unrated",
+      totalProblemsSolved: problemSolvedSet.size,
+    };
+
+    res.json(codeforcesData);
   } catch (err) {
-    res.status(500).json({ error: "Failed to fetch Codeforces data" });
+    res
+      .status(500)
+      .json({ message: "Failed to fetch Codeforces data", error: err });
   }
 });
 
