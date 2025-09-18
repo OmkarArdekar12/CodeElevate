fb1a4511c66d4365027f6dea931a73c9a674848b
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-18 15:37:21 +0530

    Working on the Competitive Stats

--- frontend/src/service/competitiveProgrammingStatsApi.js
+++ frontend/src/service/competitiveProgrammingStatsApi.js
@@ -1,6 +1,15 @@
 import api from "./api";
 
 export const leetCodeStats = async (username) => {
-  const userLeetCodeData = api.get(`/leetcode/${username}`);
+  const userLeetCodeData = api.get(
+    `/stats/competitive-programming/leetcode/${username}`
+  );
   return userLeetCodeData;
+};
+
+export const codeforcesStats = async (username) => {
+  const userCodeforcesData = api.get(
+    `/stats/competitive-programming/codeforces/${username}`
+  );
+  return userCodeforcesData;
 };
