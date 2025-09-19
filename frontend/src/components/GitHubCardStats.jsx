16c1a50b724e842b7e25867cccb9dfc996f7fec2
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-19 23:18:42 +0530

    Working on Github Stats

--- frontend/src/components/GitHubCardStats.jsx
+++ frontend/src/components/GitHubCardStats.jsx
@@ -1,49 +1,36 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+
+const gitHubData = {
+  userId: "OmkarArdekar12",
+  name: "Omkar Prakash Ardekar",
+  avatarUrl: "https://avatars.githubusercontent.com/u/178113083?v=4",
+  bio: "Mastering core fundamentals concepts to advanced concepts through continuous learning",
+  publicRepos: 9,
+  followers: 0,
+  following: 0,
+  totalStars: 0,
+};
 
 const GitHubCardStats = () => {
   return (
     <div className="w-[90%] lg:w-[37%] m-1 my-4 p-6 text-white bg-gray-900 rounded-2xl shadow-xl border border-gray-100">
       <div className="flex flex-wrap items-center justify-center">
-        <img
-          src="/images/LeetCodeLogo.png"
-          alt="LeetCode"
-          className="w-9 h-9 mr-2"
-        />
+        <FaGithub />
         <span className="text-white text-2xl text-border">
-          {leetCodeData.username}
+          {gitHubData.userId}
         </span>
       </div>
       <hr className="text-gray-500 my-4" />
-      <div className="flex items-center mb-3">
-        <div className="flex gap-2">
-          {leetCodeData.hasGuardian && (
-            <div className="flex flex-wrap items-center justify-center">
-              <img
-                src="/images/LeetCodeGuardianBadge.png"
-                alt="Guardian"
-                className="h-12 mr-2"
-              />
-              <span
-                className={`px-3 py-0.5 font-bold rounded-full ${badgeColors.Guardian}`}
-              >
-                Guardian
-              </span>
-            </div>
-          )}
-          {leetCodeData.hasKnight && (
-            <div className="flex flex-wrap items-center justify-center">
-              <img
-                src="/images/LeetCodeKnightBadge.png"
-                alt="Knight"
-                className="h-12 mr-2"
-              />
-              <span
-                className={`px-3 py-0.5 font-bold rounded-full ${badgeColors.Knight}`}
-              >
-                Knight
-              </span>
-            </div>
-          )}
+      <div className="flex flex-wrap justify-center items-center mb-3">
+        {gitHubData.avatarUrl && (
+          <div className="flex items-center flex-wrap">
+            <img src={gitHubData.avatarUrl} alt="githubProfileImage" />
+          </div>
+        )}
+        <div className="flex flex-col justify-center items-center flex-wrap">
+          {gitHubData.name && <h3 className="text-xl">{gitHubData.name}</h3>}
+          {gitHubData.bio && <h5 className="text-sm">{gitHubData.bio}</h5>}
         </div>
       </div>
       <div className="flex flex-wrap items-center gap-2 mb-3">
