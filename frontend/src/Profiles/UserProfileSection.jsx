7c5a2b9767ecdd9b60d354fb1b28043b8cca45e6
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-20 23:23:36 +0530

    User Profile Section

--- frontend/src/Profiles/UserProfileSection.jsx
+++ frontend/src/Profiles/UserProfileSection.jsx
@@ -17,15 +17,19 @@ const UserProfileSection = ({
         />
       </div>
       <div className="w-full flex flex-col items-center relative -mt-20 z-1 md:flex-row md:pl-20">
-        <img
-          src={profilePicture ? profilePicture : "/images/userImage.png"}
-          alt="userImage"
-          className="w-39 h-39 flex items-center justify-center md:w-70 md:h-70 rounded-full border-3 border-white object-cover"
-        />
+        <div className="flex flex-col items-center">
+          <img
+            src={profilePicture ? profilePicture : "/images/userImage.png"}
+            alt="userImage"
+            className="w-39 h-39 flex items-center justify-center md:w-70 md:h-70 rounded-full border-3 border-white object-cover"
+          />
+          {username && (
+            <h3 className="text-white text-md italic mt-3">@{username}</h3>
+          )}
+        </div>
         <div className="flex flex-col justify-center items-center md:items-start mt-10 md:ml-4 text-white text-5xl font-semibold">
           {fullName && <h1>{fullName}</h1>}
-          {username && <h1>@{username}</h1>}
-          {headLine && <h2 className="mt-1 text-4xl">{headLine}</h2>}
+          {headLine && <h2 className="mt-1 text-3xl font-sans">{headLine}</h2>}
         </div>
       </div>
     </>
