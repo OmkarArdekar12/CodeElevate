b8999b6117a3b86c9e5e68362603f8b821e0c28c
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-21 21:44:04 +0530

    Working on Domain and Role Section

--- frontend/src/Profiles/DomainRoleSection.jsx
+++ frontend/src/Profiles/DomainRoleSection.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleRight } from "@fortawesome/free-solid-svg-icons";
 
-const DomainRoleSection = () => {
+const DomainRoleSection = ({ domain, role }) => {
   return (
     <div className="w-full flex flex-col justify-center p-4">
       <h2 className="text-3xl mb-3 title-font">Domain & Role</h2>
       <div className="w-full flex items-center flex-wrap">
-        <h3 className="bg-indigo-500 inline px-4 py-2 rounded-lg text-lg mx-2 my-1 hover-text-border text-center">
-          <FontAwesomeIcon icon={faCircleRight} className="text-black mr-1" />
-          Role: SDE
-        </h3>
-        <h3 className="bg-teal-500 inline px-4 py-2 rounded-lg text-lg mx-2 my-1 hover-text-border text-center align-middle">
-          <FontAwesomeIcon icon={faCircleRight} className="text-black mr-1" />
-          Domain: AWS
-        </h3>
+        {role && (
+          <h3 className="bg-indigo-500 inline px-4 py-2 rounded-lg text-lg mx-2 my-1 hover-text-border text-center">
+            <FontAwesomeIcon icon={faCircleRight} className="text-black mr-1" />
+            Role: {role.toUpperCase()}
+          </h3>
+        )}
+        {domain && (
+          <h3 className="bg-teal-500 inline px-4 py-2 rounded-lg text-lg mx-2 my-1 hover-text-border text-center align-middle">
+            <FontAwesomeIcon icon={faCircleRight} className="text-black mr-1" />
+            Domain: {domain.toUpperCase()}
+          </h3>
+        )}
       </div>
     </div>
   );
