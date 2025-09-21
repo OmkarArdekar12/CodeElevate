4dca6b63d915b8d3b52f194d80444d72d1dec4dc
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-21 22:39:57 +0530

    Working on Version control Profiles

--- frontend/src/Profiles/DevelopmentSection.jsx
+++ frontend/src/Profiles/DevelopmentSection.jsx
@@ -10,58 +10,69 @@ const DevelopmentSection = ({ versionControlProfiles }) => {
   const github = versionControlProfiles.github;
   const gitlab = versionControlProfiles.gitlab;
   const portfolio = versionControlProfiles.portfolio;
+  const isEmpty = github || gitlab || portfolio;
   return (
     <>
-      <div className="w-full flex flex-col justify-center p-4">
-        <h2 className="text-3xl mb-1 title-font">Development Profiles</h2>
-        <div className="mt-4 p-4 md:px-25">
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-            <div className="flex items-center space-x-2 flex-wrap">
-              <FaCheckCircle className="text-blue-500 w-5 h-5 inline" />
-              <div className="flex items-center space-x-1 flex-wrap group">
-                <FaGithub className="text-white w-9 h-9" />
-                <a
-                  href={`https://github.com/${github}`}
-                  target="_blank"
-                  className="text-2xl hover-text-border hover:underline hover:decoration-[#FFA116] hover:decoration-2"
-                >
-                  GitHub
-                </a>
-                <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
-              </div>
-            </div>
-            <div className="flex items-center space-x-2 flex-wrap">
-              <FaCheckCircle className="text-blue-500 w-5 h-5 inline" />
-              <div className="flex items-center space-x-1 flex-wrap group">
-                <FaGitlab className="text-[#FC6D26] w-9 h-9" />
-                <a
-                  href={`https://gitlab.com/${gitlab}`}
-                  target="_blank"
-                  className="text-2xl hover-text-border hover:underline hover:decoration-[#FFA116] hover:decoration-2"
-                >
-                  GitLab
-                </a>
-                <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
-              </div>
-            </div>
-            <div className="flex items-center space-x-2 flex-wrap">
-              <FaCheckCircle className="text-blue-500 w-5 h-5" />
-              <div className="flex items-center space-x-1 flex-wrap group">
-                <FaLaptopCode className="text-[#2563EB] w-9 h-9" />
-                <a
-                  href={`${portfolio}`}
-                  target="_blank"
-                  className="text-2xl hover-text-border hover:underline hover:decoration-[#1F8ACB] hover:decoration-2"
-                >
-                  Portfolio
-                </a>
-                <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+      {isEmpty && (
+        <>
+          <div className="w-full flex flex-col justify-center p-4">
+            <h2 className="text-3xl mb-1 title-font">Development Profiles</h2>
+            <div className="mt-4 p-4 md:px-25">
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+                {github && (
+                  <div className="flex items-center space-x-2 flex-wrap">
+                    <FaCheckCircle className="text-blue-500 w-5 h-5 inline" />
+                    <div className="flex items-center space-x-1 flex-wrap group">
+                      <FaGithub className="text-white w-9 h-9" />
+                      <a
+                        href={`https://github.com/${github}`}
+                        target="_blank"
+                        className="text-2xl hover-text-border hover:underline hover:decoration-[#FFA116] hover:decoration-2"
+                      >
+                        GitHub
+                      </a>
+                      <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                    </div>
+                  </div>
+                )}
+                {gitlab && (
+                  <div className="flex items-center space-x-2 flex-wrap">
+                    <FaCheckCircle className="text-blue-500 w-5 h-5 inline" />
+                    <div className="flex items-center space-x-1 flex-wrap group">
+                      <FaGitlab className="text-[#FC6D26] w-9 h-9" />
+                      <a
+                        href={`https://gitlab.com/${gitlab}`}
+                        target="_blank"
+                        className="text-2xl hover-text-border hover:underline hover:decoration-[#FFA116] hover:decoration-2"
+                      >
+                        GitLab
+                      </a>
+                      <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                    </div>
+                  </div>
+                )}
+                {portfolio && (
+                  <div className="flex items-center space-x-2 flex-wrap">
+                    <FaCheckCircle className="text-blue-500 w-5 h-5" />
+                    <div className="flex items-center space-x-1 flex-wrap group">
+                      <FaLaptopCode className="text-[#2563EB] w-9 h-9" />
+                      <a
+                        href={`${portfolio}`}
+                        target="_blank"
+                        className="text-2xl hover-text-border hover:underline hover:decoration-[#1F8ACB] hover:decoration-2"
+                      >
+                        Portfolio
+                      </a>
+                      <FiExternalLink className="w-4 h-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <hr className="w-full text-gray-600 my-10" />
+          <hr className="w-full text-gray-600 my-10" />
+        </>
+      )}{" "}
     </>
   );
 };
