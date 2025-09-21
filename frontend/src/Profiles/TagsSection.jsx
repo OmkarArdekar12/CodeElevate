5467ac58833fbe2689b01386aabfdec350273d03
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-21 22:07:14 +0530

    Working on Tags Section

--- frontend/src/Profiles/TagsSection.jsx
+++ frontend/src/Profiles/TagsSection.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 
-const TagsSection = () => {
-  const tags = ["Coder", "Learner", "Pro"];
+const TagsSection = ({ tags }) => {
   return (
-    <div className="w-full flex flex-col justify-center p-4">
-      <h2 className="text-3xl mb-3 title-font">Tags</h2>
-      <div className="w-full flex items-center flex-wrap">
-        {tags.map((tag, index) => (
-          <span
-            key={index}
-            className="bg-gradient-to-r from-gray-500 via-gray-600 to-gray-700 shadow-cyan-300 text-white inline m-1 py-1 px-2 rounded-md"
-          >
-            {tag}
-          </span>
-        ))}
-      </div>
-    </div>
+    <>
+      {tags.length != 0 && (
+        <div className="w-full flex flex-col justify-center p-4">
+          <h2 className="text-3xl mb-3 title-font">Tags</h2>
+          <div className="w-full flex items-center flex-wrap">
+            {tags.map((tag, index) => (
+              <span
+                key={index}
+                className="bg-gradient-to-r from-gray-500 via-gray-600 to-gray-700 shadow-cyan-300 text-white inline m-1 py-1 px-2 rounded-md"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
