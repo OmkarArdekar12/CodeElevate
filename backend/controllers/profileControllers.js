9a599d153aecac6dfa4d5d4a7f88c0a91e59c73e
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-23 23:02:17 +0530

    Profiles Controller

--- backend/controllers/profileControllers.js
+++ backend/controllers/profileControllers.js
@@ -13,8 +13,8 @@ export const getAllProfiles = async (req, res) => {
 
 export const showProfile = async (req, res) => {
   try {
-    const id = req.params.id;
-    const profile = await Profile.findOne({ _id: id }).populate(
+    const userId = req.params.id;
+    const profile = await Profile.findOne({ user: userId }).populate(
       "user",
       "username"
     );
@@ -26,26 +26,6 @@ export const showProfile = async (req, res) => {
     res.status(500).json({ message: "Error in fetching profile", error: err });
   }
 };
-
-// export const createProfile = async (req, res) => {
-//   try {
-//     const userId = req.user._id;
-//     let profile = await Profile.findOne({ user: userId });
-//     if (profile) {
-//       profile = Object.assign(profile, req.body);
-//       await profile.save();
-//       return res.status(200).json(profile);
-//     }
-
-//     profile = new Profile({ user: userId, ...req.body });
-//     await profile.save();
-//     res.status(200).json(profile);
-//   } catch (err) {
-//     res
-//       .status(500)
-//       .json({ message: "Error in creating/updating profile", error: err });
-//   }
-// };
 
 export const updateProfile = async (req, res) => {
   try {
@@ -89,3 +69,39 @@ export const destroyProfile = async (req, res) => {
     res.status(500).json({ message: "Error deleting profile" });
   }
 };
+
+// export const showProfile = async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const profile = await Profile.findOne({ _id: id }).populate(
+//       "user",
+//       "username"
+//     );
+//     if (!profile) {
+//       return res.status(404).json({ message: "Profile not found" });
+//     }
+//     res.status(200).json(profile);
+//   } catch (err) {
+//     res.status(500).json({ message: "Error in fetching profile", error: err });
+//   }
+// };
+
+// export const createProfile = async (req, res) => {
+//   try {
+//     const userId = req.user._id;
+//     let profile = await Profile.findOne({ user: userId });
+//     if (profile) {
+//       profile = Object.assign(profile, req.body);
+//       await profile.save();
+//       return res.status(200).json(profile);
+//     }
+
+//     profile = new Profile({ user: userId, ...req.body });
+//     await profile.save();
+//     res.status(200).json(profile);
+//   } catch (err) {
+//     res
+//       .status(500)
+//       .json({ message: "Error in creating/updating profile", error: err });
+//   }
+// };
