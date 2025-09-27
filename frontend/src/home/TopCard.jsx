9e44fe4b5ce150c45f623f1036996368d7b1c250
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-27 08:33:20 +0530

    Working on UI

--- frontend/src/home/TopCard.jsx
+++ frontend/src/home/TopCard.jsx
@@ -1,10 +1,12 @@
 export default function TopCard({ image, text }) {
   return (
-    <div className="TopCard flex items-center justify-center px-7 py-5 w-[26rem] h-45 rounded-lg border border-yellow-200 mx-10 my-1">
+    <div className="TopCard group flex items-center justify-center px-7 py-5 w-[26rem] h-45 rounded-lg border border-yellow-200 mx-10 my-1 cursor-pointer">
       <img src={image} alt={text} className="h-[100%] mx-2" />
       <div>
         <h1 className="font-bold text-4xl">Top</h1>
-        <h1 className="font-bold text-4xl">{text}</h1>
+        <h1 className="font-bold text-4xl group-hover:text-[2.5rem] transition-all duration-300 ease-in-out">
+          {text}
+        </h1>
       </div>
     </div>
   );
