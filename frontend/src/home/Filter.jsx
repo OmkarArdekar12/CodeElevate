cba25100c4be62687b0504eb3c8a8e2a4b15e113
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-27 08:25:26 +0530

    Working on UI

--- frontend/src/home/Filter.jsx
+++ frontend/src/home/Filter.jsx
@@ -1,6 +1,6 @@
 export default function Filter({ text }) {
   return (
-    <div className="text-white text-sm border border-gray-500 rounded-full my-1 mx-2 px-5 py-2 text-center hover:bg-green-600 hover:border-green-600">
+    <div className="text-white text-sm border border-gray-500 rounded-full my-1 mx-2 px-5 py-2 text-center hover:bg-green-600 hover:border-green-600 cursor-pointer">
       <p>{text}</p>
     </div>
   );
