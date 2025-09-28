50a6b6db3780bd5add2553aef48166f7c260508f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-28 23:33:54 +0530

    Working on Posts Feature

--- frontend/src/posts/PostCard.jsx
+++ frontend/src/posts/PostCard.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const PostCard = () => {
   return (
     <div className="flex flex-col items-center">
-      <div></div>
+      <div>Title</div>
+      <div>Description</div>
+      <div>Image</div>
     </div>
   );
 };
