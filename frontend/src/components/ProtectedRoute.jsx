d37bbeb698f386517efb3ca1f87b327a0995917a
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-04 12:59:51 +0530

    Protected Route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useSession } from "../context/SessionContext";
 import Loading from "./Loading";
+import toast from "react-hot-toast";
 
 const ProtectedRoute = () => {
   const { isLoggedIn, loading } = useSession();
@@ -9,7 +10,16 @@ const ProtectedRoute = () => {
   if (loading) {
     return <Loading />;
   }
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
+  return isLoggedIn ? (
+    <Outlet />
+  ) : (
+    <>
+      {toast.error("You must be logged-in to access that.", {
+        id: "Unauthorized Access",
+      })}
+      <Navigate to="/login" replace />
+    </>
+  );
 };
 
 export default ProtectedRoute;
