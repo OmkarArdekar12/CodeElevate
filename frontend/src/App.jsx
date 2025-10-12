8b2e356dc9ae6cba0c83290e2ad1bda266538d3d
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-12 23:36:48 +0530

    Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,23 +12,3 @@ function App() {
 }
 
 export default App;
-
-// import "./App.css";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./routes.jsx";
-// import { SessionProvider } from "./context/SessionContext.jsx";
-// function App() {
-//   return (
-//     <SessionProvider>
-//       <Toaster
-//         position="top-right"
-//         toastOptions={{
-//           duration: 3000,
-//           style: { background: "#333", color: "#fff" },
-//         }}
-//       />
-//       <RouterProvider router={router} />
-//     </SessionProvider>
-//   );
-// }
-// export default App;
