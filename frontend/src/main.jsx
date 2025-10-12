93ac9c4657726b0ca25d402afa5bc9b113cddad8
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-12 23:38:12 +0530

    Completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,3 +10,21 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import { Toaster } from "react-hot-toast";
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <Toaster
+//       position="top-right"
+//       toastOptions={{
+//         duration: 3000,
+//         style: { background: "#333", color: "#fff" },
+//       }}
+//     />
+//     <App />
+//   </StrictMode>
+// );
