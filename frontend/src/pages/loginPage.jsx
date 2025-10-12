f6c69b6b535d85d4dabf2dce674bf01b4c4f3a9a
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-12 23:57:46 +0530

    Completed

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React from "react";
 import LoginForm from "../components/LoginForm";
 import { useNavigate } from "react-router-dom";
 import { useSession } from "../context/SessionContext";
