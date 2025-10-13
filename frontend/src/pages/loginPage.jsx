8631a3e034d59c7a9fc1acaa0838b23ca25f4ba3
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-13 17:55:19 +0530

    Completed

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import LoginForm from "../components/LoginForm";
-import { useNavigate } from "react-router-dom";
-import { useSession } from "../context/SessionContext";
+import React, { useState, useEffect } from "react";
+import LoginForm from "../components/LoginForm.jsx";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSession } from "../context/SessionContext.jsx";
+import { motion } from "framer-motion";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const { login } = useSession();
+  const location = useLocation();
+  const [logoSource, setLogoSource] = useState("");
+
+  useEffect(() => {
+    const timestamp = new Date().getTime();
+    setLogoSource(`/images/CodeElevateLogo.gif?t=${timestamp}`);
+  }, [location.pathname]);
 
   const handleLoginSuccess = (userData) => {
-    console.log("The logged in userData: ", userData);
+    // console.log("The logged in userData: ", userData);
     login(userData);
     if (!userData.isMfaActive) {
       navigate("/setup-2fa");
@@ -19,14 +27,24 @@ const LoginPage = () => {
 
   return (
     <div className="w-[100%] h-[100%] text-white bg-slate-900 flex flex-col justify-center items-center px-5 py-9 transition-all duration-300 ease-in-out">
-      <div className="inline-flex justify-center items-center mb-5">
-        <img
-          src="/images/CodeElevateLogo.gif"
-          alt="CodeElevate"
-          className="h-25 mr-1"
-        />
-        <h1 className="logo-text ml-1 text-white text-4xl">CodeElevate</h1>
-      </div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.1, ease: "easeInOut" }}
+        className="inline-flex flex-col justify-center items-center mb-5 md:flex-row"
+      >
+        {logoSource && (
+          <img
+            key={logoSource}
+            src={logoSource}
+            alt="CodeElevate"
+            className="h-25 mr-1"
+          />
+        )}
+        <h1 className="logo-text ml-1 text-white text-3xl md:text-4xl">
+          CodeElevate
+        </h1>
+      </motion.div>
       <LoginForm onLoginSuccess={handleLoginSuccess} />
     </div>
   );
