5fb74ba3f6587bbf5ce81473d27bb8395aa81a49
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-16 22:49:40 +0530

    Completed

--- backend/routes/connectionRoutes.js
+++ backend/routes/connectionRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import auth from "../middlewares/auth.js";
 import verifyAuth from "../middlewares/verifyAuth.js";
+import auth2FA from "../middlewares/auth2FA.js";
 import {
   followUser,
   respondConnectRequest,
@@ -13,21 +14,27 @@ import {
 const router = express.Router();
 
 //Follow Route
-router.post("/follow/:id", auth, verifyAuth, followUser);
+router.post("/follow/:id", auth, verifyAuth, auth2FA, followUser);
 
 //Unfollow Route
-router.post("/unfollow/:id", auth, verifyAuth, unfollowUser);
+router.post("/unfollow/:id", auth, verifyAuth, auth2FA, unfollowUser);
 
 //Connect Route
-router.post("/connect/:id", auth, verifyAuth, sendConnectRequest);
+router.post("/connect/:id", auth, verifyAuth, auth2FA, sendConnectRequest);
 
 //Connect Respond Route
-router.post("/connect/response/:id", auth, verifyAuth, respondConnectRequest);
+router.post(
+  "/connect/response/:id",
+  auth,
+  verifyAuth,
+  auth2FA,
+  respondConnectRequest
+);
 
 //Unconnect Route
-router.post("/unconnect/:id", auth, verifyAuth, unconnectUser);
+router.post("/unconnect/:id", auth, verifyAuth, auth2FA, unconnectUser);
 
 //Check Connection Status Route
-router.get("/:id/status", auth, verifyAuth, checkConnectionStatus);
+router.get("/:id/status", auth, verifyAuth, auth2FA, checkConnectionStatus);
 
 export default router;
