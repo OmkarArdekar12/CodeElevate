2ea59f94bf7ab18392589fc05f5968be6c01e51d
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-16 18:23:59 +0530

    Completed

--- frontend/src/about/AboutPage.jsx
+++ frontend/src/about/AboutPage.jsx
@@ -1,6 +1,6 @@
 export default function AboutPage() {
   return (
-    <div>
+    <div className="w-full flex flex-col items-center justify-center text-white py-4 px-2 md:px-10 mb-5 transition-all duration-300 ease-in-out">
       <h1>About Section</h1>
     </div>
   );
