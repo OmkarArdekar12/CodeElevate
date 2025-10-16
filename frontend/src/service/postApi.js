a27e8261e0db74c1b7d9754548f7f7b685853fab
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-16 23:02:48 +0530

    Completed

--- frontend/src/service/postApi.js
+++ frontend/src/service/postApi.js
@@ -2,15 +2,23 @@ import api from "./api";
 import { getToken2fa } from "./utils/getToken2FA.js";
 
 export const createPost = async (post) => {
+  const token2FA = getToken2fa();
   const response = await api.post("/posts", post, {
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+      "Content-Type": "multipart/form-data",
+    },
     withCredentials: true,
   });
   return response.data;
 };
 
 export const editPost = async (id, post) => {
+  const token2FA = getToken2fa();
   const response = await api.put(`/posts/${id}/edit`, post, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
     withCredentials: true,
   });
   return response.data;
@@ -34,27 +42,45 @@ export const getPost = async (id) => {
 };
 
 export const likeOrUnlikePost = async (id) => {
+  const token2FA = getToken2fa();
   const response = await api.patch(`/posts/${id}/like`, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
     withCredentials: true,
   });
   return response.data;
 };
 
 export const addComment = async (id, comment) => {
+  const token2FA = getToken2fa();
   const response = await api.put(`/posts/${id}/comment`, comment, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
     withCredentials: true,
   });
   return response.data;
 };
 
 export const deleteComment = async (postId, commentId) => {
+  const token2FA = getToken2fa();
   const response = await api.delete(`/posts/${postId}/comment/${commentId}`, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
     withCredentials: true,
   });
   return response.data;
 };
 
 export const deletePost = async (id) => {
-  const response = await api.delete(`/posts/${id}`, { withCredentials: true });
+  const token2FA = getToken2fa();
+  const response = await api.delete(`/posts/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
+    withCredentials: true,
+  });
   return response.data;
 };
