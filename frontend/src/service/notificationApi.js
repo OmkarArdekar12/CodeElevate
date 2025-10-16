c03aa81eb4fdd699a664f4f18cce2b5edda791c4
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-16 22:53:48 +0530

    Completed

--- frontend/src/service/notificationApi.js
+++ frontend/src/service/notificationApi.js
@@ -1,21 +1,34 @@
 import api from "./api";
+import { getToken2fa } from "./utils/getToken2FA";
 
 export const getNotifications = async () => {
+  const token2FA = getToken2fa();
   const response = await api.get("/notifications", {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
     withCredentials: true,
   });
   return response.data;
 };
 
 export const markNotificationAsRead = async (id) => {
+  const token2FA = getToken2fa();
   const response = await api.patch(`/notifications/${id}/read`, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
     withCredentials: true,
   });
   return response.data;
 };
 
 export const deleteNotification = async (id) => {
+  const token2FA = getToken2fa();
   const response = await api.delete(`/notifications/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
     withCredentials: true,
   });
   return response.data;
