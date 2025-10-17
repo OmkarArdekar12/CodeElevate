06e50db74f3bdb290e4b7ea76f8d58efb0f28348
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-17 21:44:45 +0530

    Completed

--- frontend/src/home/FilterSection.jsx
+++ frontend/src/home/FilterSection.jsx
@@ -4,10 +4,10 @@ export default function FilterSection() {
   return (
     <div className="FilterSection w-[45%] flex flex-wrap justify-center items-center transition-all duration-300 ease-in-out">
       <Filter text={"Trending"} />
-      <Filter text={"Student"} />
+      <Filter text={"CP/DSA"} />
+      <Filter text={"DEVS"} />
+      <Filter text={"Rankers"} />
       <Filter text={"Professionals"} />
-      <Filter text={"DSA"} />
-      <Filter text={"Development"} />
     </div>
   );
 }
