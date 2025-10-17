f28419dede7eb4d78212ae7a88055523f5c4f888
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-17 23:22:38 +0530

    Completed

--- frontend/src/about/Features.jsx
+++ frontend/src/about/Features.jsx
@@ -5,7 +5,7 @@ const Features = () => {
     <div className="w-full flex flex-col mt-2 justify-center items-center">
       <h1 className="text-md md:text-3xl">Features</h1>
       <div className="w-full flex flex-col justify-center items-center">
-        <div></div>
+        <div className="w-full flex items-center"></div>
       </div>
     </div>
   );
