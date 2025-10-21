a8fd7c1f6ff39a4ec495380aff058e1cf88951e7
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-21 08:09:33 +0530

    Completed

---
+++ frontend/src/service/messagesApi.js
@@ -0,0 +1,35 @@
+import api from "./api";
+import { getToken2fa } from "./utils/getToken2FA.js";
+
+export const getUserForSidebar = async () => {
+  const token2FA = getToken2fa();
+  const response = await api.get("/messages/api/users", {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
+    withCredentials: true,
+  });
+  return response.data;
+};
+
+export const getMessages = async (id) => {
+  const token2FA = getToken2fa();
+  const response = await api.get(`/messages/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
+    withCredentials: true,
+  });
+  return response.data;
+};
+
+export const sendMessage = async (id, meesageData) => {
+  const token2FA = getToken2fa();
+  const response = await api.post(`/messages/${id}`, meesageData, {
+    headers: {
+      Authorization: `Bearer ${token2FA}`,
+    },
+    withCredentials: true,
+  });
+  return response.data;
+};
