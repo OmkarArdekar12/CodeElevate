7eafc11942c959112fd4b54318fbf5dd3f9d71ee
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-23 13:34:15 +0530

    Search functionality completed

--- frontend/src/home/Search.jsx
+++ frontend/src/home/Search.jsx
@@ -1,7 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export default function Search({ searchTerm, setSearchTerm }) {
+  const [placeholderText, setPlaceholderText] = useState("");
+  const [placeholderIdx, setPlaceholderIdx] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+  const [typingForward, setTypingForward] = useState(true);
+  const placeholders = [
+    "Search here...",
+    "Search by fullname...",
+    "Search by username...",
+    "Search by role...",
+    "Search by domain...",
+    "Search by tags...",
+    "Search by your interest...",
+  ];
+  const size = placeholders.length;
+
+  useEffect(() => {
+    const typingSpeed = 100;
+    const pauseTime = 1000;
+
+    const interval = setInterval(
+      () => {
+        const currentText = placeholders[placeholderIdx];
+
+        if (typingForward) {
+          setCharIndex((prev) => {
+            if (prev < currentText.length) {
+              setPlaceholderText(currentText.slice(0, prev + 1));
+              return prev + 1;
+            } else {
+              setTypingForward(false);
+              return prev;
+            }
+          });
+        } else {
+          setCharIndex((prev) => {
+            if (prev > 0) {
+              setPlaceholderText(currentText.slice(0, prev - 1));
+              return prev - 1;
+            } else {
+              setTypingForward(true);
+              setPlaceholderIdx((prevIndex) => (prevIndex + 1) % size);
+              return 0;
+            }
+          });
+        }
+      },
+      typingForward && charIndex === placeholders[placeholderIdx].length
+        ? pauseTime
+        : typingSpeed
+    );
+
+    return () => clearInterval(interval);
+  }, [charIndex, typingForward, placeholderIdx]);
+
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -18,7 +72,7 @@ export default function Search({ searchTerm, setSearchTerm }) {
       >
         <input
           type="text"
-          placeholder="Search here..."
+          placeholder={placeholderText}
           className="text-white border border-cyan-300 rounded text-md p-2 w-[93%] focus:outline-none focus:ring-1 focus:ring-[#00c4cc]"
           value={searchTerm}
           onChange={handleChange}
