bf32149a0cacba614d576a0c49bfb2c3ca28e840
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-23 13:11:42 +0530

    Proctecting Private Route Completed

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useSession } from "../context/SessionContext.jsx";
 import Loading from "./Loading.jsx";
@@ -6,12 +6,29 @@ import toast from "react-hot-toast";
 
 const PrivateRoute = () => {
   const { isLoggedIn, isVerified, loading } = useSession();
+
+  useEffect(() => {
+    if (!loading) {
+      if (!isLoggedIn) {
+        toast.error("You need to be logged in to access that!", {
+          id: "private route login",
+        });
+      } else if (!isVerified) {
+        toast.error("You need to verify your account to continue!", {
+          id: "private route verify",
+        });
+      }
+    }
+  }, [loading, isLoggedIn, isVerified]);
+
   if (loading) {
     return <Loading />;
   }
+
   if (isLoggedIn && !isVerified) {
     return <Navigate to="/verify-2fa" replace />;
   }
+
   return isLoggedIn && isVerified ? (
     <Outlet />
   ) : (
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,10 +3,19 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSession } from "../context/SessionContext.jsx";
 import Loading from "./Loading.jsx";
 import toast from "react-hot-toast";
+import { useEffect } from "react";
 
 const ProtectedRoute = () => {
   const { isLoggedIn, loading } = useSession();
-  // console.log("The LoggedIn: ", isLoggedIn);
+
+  useEffect(() => {
+    if (!loading && !isLoggedIn) {
+      toast.error("You need to be logged in to access that!", {
+        id: "proctected route login",
+      });
+    }
+  }, [loading, isLoggedIn]);
+
   if (loading) {
     return <Loading />;
   }
