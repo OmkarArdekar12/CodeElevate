f5ccad819b1858b28d2d3d90266bbb1d0f0bd98f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-24 16:45:22 +0530

    Completed

--- frontend/src/service/socketService.js
+++ frontend/src/service/socketService.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL =
+  import.meta.env.VITE_BACKEND_URL || "https://codeelevate.onrender.com";
+// const BASE_URL = "http://localhost:8080";
 
 const socket = io(BASE_URL, { withCredentials: true });
 
