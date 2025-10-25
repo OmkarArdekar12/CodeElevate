773c635fdc500b9ef30dd1263746209f9fbe9e36
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-25 09:10:43 +0530

    Completed

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_BACKEND_URL
-  //   ? `${import.meta.env.VITE_BACKEND_URL}/api`
-  //   : "https://codeelevate.onrender.com/api", //live backend url
-  baseURL: `http://localhost:8080/api`, //backend url
+  baseURL: import.meta.env.VITE_BACKEND_URL
+    ? `${import.meta.env.VITE_BACKEND_URL}/api`
+    : "https://codeelevate.onrender.com/api", //live backend url
+  // baseURL: `http://localhost:8080/api`, //backend url
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
