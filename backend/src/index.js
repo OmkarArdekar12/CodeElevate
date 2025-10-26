aee4da1295f6e86b928498e02ce5eff1b53a7b8d
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-26 13:15:21 +0530

    Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -89,7 +89,6 @@ const sessionOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   },
 };
