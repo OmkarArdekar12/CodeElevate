b3a2ca00237f437e680273e6244b77f87fd5d23a
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-26 15:37:30 +0530

    Completed

--- backend/src/middlewares/profileValidations.js
+++ backend/src/middlewares/profileValidations.js
@@ -30,7 +30,11 @@ export const isOwner = async (req, res, next) => {
     if (profile.user.toString() !== clientUserId.toString()) {
       return res
         .status(403)
-        .json({ message: "You are not authorized to edit this profile" });
+        .json({
+          message: "You are not authorized to edit this profile",
+          profileUserId,
+          clientUserId,
+        });
     }
 
     next();
