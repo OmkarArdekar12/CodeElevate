707739055beedc7eeec32f5eca3042503c1448cd
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-27 20:57:37 +0530

    Realtime messaging system

---
+++ frontend/src/about/RealTimeMessagingSystemFeature.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RealTimeMessagingSystemFeature = () => {
+  return <div>RealTimeMessagingSystemFeature</div>;
+};
+
+export default RealTimeMessagingSystemFeature;
