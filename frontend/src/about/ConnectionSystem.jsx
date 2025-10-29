f1b65b7d190c27acee1fb6640c6f426234411e63
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 15:16:01 +0530

    Connection System (follow, unfollow, connect) Completed

--- frontend/src/about/ConnectionSystem.jsx
+++ frontend/src/about/ConnectionSystem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const ConnectionSystem = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row items-center px-4 gap-2 py-10 text-gray-200">
-      <div className="flex items-center justify-center w-full md:w-1/2">
-        <img
-          src="/about/connectionProcess.png"
-          alt="Connection Process Image"
-          className="w-full"
-        />
-      </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 0.2, ease: "easeOut" }}
+      className="w-full flex flex-col md:flex-row items-center px-4 gap-2 py-10 text-gray-200 transition-all duration-300 ease-in-out"
+    >
       <div className="flex flex-col items-center justify-center w-full md:w-1/2 text-md md:text-xl px-2 md:px-14 gap-3">
         <h2 className="text-xl md:text-3xl font-semibold text-center">
           Connection System
@@ -26,7 +26,14 @@ const ConnectionSystem = () => {
           deeper networking within the community.
         </p>
       </div>
-    </div>
+      <div className="flex items-center justify-center w-full md:w-1/2">
+        <img
+          src="/about/connectionProcess.png"
+          alt="Connection Process Image"
+          className="w-full about-image transition-transform duration-300 ease-in-out hover:scale-105"
+        />
+      </div>
+    </motion.div>
   );
 };
 
