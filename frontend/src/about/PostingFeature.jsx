79bf0af319dfccd53527544596de772fedd161fe
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 15:13:25 +0530

    Post System and Feature Completed

--- frontend/src/about/PostingFeature.jsx
+++ frontend/src/about/PostingFeature.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const PostingFeature = () => {
   return (
-    <div className="w-full flex flex-col items-center px-4 gap-2 py-10 text-gray-200">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 0.2, ease: "easeOut" }}
+      className="w-full flex flex-col items-center px-4 gap-2 py-10 text-gray-200 transition-all duration-300 ease-in-out"
+    >
       <div className="flex flex-col items-center justify-center w-full text-md md:text-xl px-2 md:px-14 gap-3">
         <h2 className="text-xl md:text-3xl font-semibold text-center">
           Post Feature
@@ -25,17 +32,17 @@ const PostingFeature = () => {
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center w-full gap-2">
         <img
-          src="/about/postFeature.png"
-          alt="Authentication Image"
-          className="w-full md:w-1/2"
-        />
-        <img
           src="/about/postOptions.png"
           alt="Authentication Image"
-          className="w-full md:w-1/2"
+          className="w-full md:w-1/2 about-image transition-transform duration-300 ease-in-out hover:scale-105"
+        />
+        <img
+          src="/about/postFeature.png"
+          alt="Authentication Image"
+          className="w-full md:w-1/2 about-image transition-transform duration-300 ease-in-out hover:scale-105"
         />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
