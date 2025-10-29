d84977bac592900a011490876e79b0c14fd20d8b
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 17:53:35 +0530

    Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,8 +10,6 @@ export default defineConfig({
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
 // import tailwindcss from "@tailwindcss/vite";
-
-// //https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [tailwindcss(), react()],
 //   server: {
