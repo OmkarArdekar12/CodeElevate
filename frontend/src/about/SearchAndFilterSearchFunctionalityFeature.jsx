62ff95d4b1db8c2b9309124221f6e83d98079323
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 14:59:13 +0530

    Search and filter search functionality

--- frontend/src/about/SearchAndFilterSearchFunctionalityFeature.jsx
+++ frontend/src/about/SearchAndFilterSearchFunctionalityFeature.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const SearchAndFilterSearchFunctionalityFeature = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row items-center px-4 gap-2 py-10 text-gray-200">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.3 }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
+      className="w-full flex flex-col md:flex-row items-center px-4 gap-2 py-10 text-gray-200 transition-all duration-300 ease-in-out"
+    >
       <div className="flex items-center justify-center w-full md:w-1/2">
         <img
           src="/about/searchFunctionality.png"
           alt="Search functionality Image"
-          className="w-full"
+          className="w-full about-image transition-transform duration-300 ease-in-out hover:scale-105"
         />
       </div>
       <div className="flex flex-col items-center justify-center w-full md:w-1/2 text-md md:text-xl px-2 md:px-14 gap-3">
@@ -29,7 +36,7 @@ const SearchAndFilterSearchFunctionalityFeature = () => {
           opportunities on the platform.
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
