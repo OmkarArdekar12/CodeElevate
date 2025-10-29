f1ef3c67b890d107183d6ec2c6dca3a0d5e20ba7
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 16:43:18 +0530

    Creator and Devloper Section Completed

--- frontend/src/about/Creator.jsx
+++ frontend/src/about/Creator.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 import ImageSection from "./ImageSection.jsx";
 import ContentSection from "./ContentSection.jsx";
 
 const Creator = () => {
   return (
-    <div className="w-full flex flex-col mt-6 justify-center items-center transition-all duration-300 ease-in-out">
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 0.1, ease: "easeOut" }}
+      className="w-full flex flex-col mt-6 justify-center items-center transition-all duration-300 ease-in-out"
+    >
       <h1 className="w-full text-center text-xl md:text-5xl border-y py-5 text-gray-100">
         Developer and Creator
       </h1>
@@ -12,7 +19,7 @@ const Creator = () => {
         <ImageSection />
         <ContentSection />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
