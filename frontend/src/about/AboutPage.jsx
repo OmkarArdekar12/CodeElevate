a38ef0c3b01d20cdf934567089fac9be62ab62f5
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 21:07:50 +0530

    Completed

--- frontend/src/about/AboutPage.jsx
+++ frontend/src/about/AboutPage.jsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
   return (
     <div className="w-full flex flex-col items-center justify-center text-white pt-14 py-4 px-2 md:px-10 mb-5 transition-all duration-300 ease-in-out">
       <div className="w-full text-2xl sm:text-3xl md:text-5xl flex items-center justify-center hover-text-border text-gray-100 border-b py-5">
-        About Code-Elevate
+        About CodeElevate
       </div>
       <Hero />
       <Features />
