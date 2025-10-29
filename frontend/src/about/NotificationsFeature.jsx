d7a3123cc44dac730f34db6ba3730376fc8a83cd
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 15:18:20 +0530

    Notification System and Feature Completed

--- frontend/src/about/NotificationsFeature.jsx
+++ frontend/src/about/NotificationsFeature.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const NotificationsFeature = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row items-center px-4 gap-2 py-10 text-gray-200">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 0.2, ease: "easeOut" }}
+      className="w-full flex flex-col md:flex-row items-center px-4 gap-2 py-10 text-gray-200 transition-all duration-300 ease-in-out"
+    >
       <div className="flex items-center justify-center w-full md:w-1/2">
         <img
           src="/about/notificationSystem.png"
           alt="Notification Feature Image"
-          className="w-full"
+          className="w-full about-image transition-transform duration-300 ease-in-out hover:scale-105"
         />
       </div>
       <div className="flex flex-col items-center justify-center w-full md:w-1/2 text-md md:text-xl px-2 md:px-14 gap-3">
@@ -25,7 +32,7 @@ const NotificationsFeature = () => {
           ensures users never miss key updates within their network.
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
