f6e1ef856e1ce5693560d9c3c771e8f7c797ba3f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 17:24:58 +0530

    Completed

--- frontend/src/service/socketService.js
+++ frontend/src/service/socketService.js
@@ -1,8 +1,12 @@
 import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-// const BASE_URL = "http://localhost:8080";
 
 const socket = io(BASE_URL, { withCredentials: true });
 
 export default socket;
+
+// import { io } from "socket.io-client";
+// const BASE_URL = "http://localhost:8080";
+// const socket = io(BASE_URL, { withCredentials: true });
+// export default socket;
