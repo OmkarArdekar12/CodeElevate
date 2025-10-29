d3f6bb48e10c64d04a3091c8ff628933c88bb73b
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-29 15:03:09 +0530

    User Centric and Multi-functionality

--- frontend/src/about/UserCentricDesgin.jsx
+++ frontend/src/about/UserCentricDesgin.jsx
@@ -6,8 +6,8 @@ const UserCentricDesgin = () => {
     <motion.div
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.3 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.7 }}
+      transition={{ duration: 0.2, ease: "easeOut" }}
       className="w-full flex flex-col md:flex-row items-center px-4 gap-2 py-10 text-gray-200 transition-all duration-300 ease-in-out"
     >
       <div className="flex flex-col items-center justify-center w-full md:w-1/2 text-md md:text-xl px-2 md:px-14 gap-3">
