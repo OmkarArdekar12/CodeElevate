fc3fe911055eaadad72168401f0b9569feb76934
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-07 09:42:19 +0530

    Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,3 +17,39 @@ export default defineConfig({
 //     open: true,
 //   },
 // });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// //https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+//   build: {
+//     chunkSizeWarningLimit: 2700,
+//   },
+// });
+
+// vercel.json
+// {
+//   "rewrites": [{ "source": "/(.*)", "destination": "/index.html" }]
+// }
+
+// vercel.json
+// {
+//   "headers": [
+//     {
+//       "source": "/sitemap.xml",
+//       "headers": [{ "key": "Content-Type", "value": "application/xml" }]
+//     },
+//     {
+//       "source": "/robots.txt",
+//       "headers": [{ "key": "Content-Type", "value": "text/plain" }]
+//     }
+//   ],
+//   "rewrites": [
+//     { "source": "/sitemap.xml", "destination": "/sitemap.xml" },
+//     { "source": "/robots.txt", "destination": "/robots.txt" },
+//     { "source": "/(.*)", "destination": "/index.html" }
+//   ]
+// }
