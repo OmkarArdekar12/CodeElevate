022f9d74373d7e12ca588a1f1ba955bb163f1f15
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-13 13:14:36 +0530

    Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,15 +9,3 @@ export default defineConfig({
     chunkSizeWarningLimit: 2700,
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-// //https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [tailwindcss(), react()],
-//   server: {
-//     port: 3000,
-//     open: true,
-//   },
-// });
