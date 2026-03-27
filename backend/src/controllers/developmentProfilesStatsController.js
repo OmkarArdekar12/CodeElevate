93e011d2988393a7d280e42df7d442da89fd6493
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-03-27 20:03:22 +0530

    DEVCache Added

--- backend/src/controllers/developmentProfilesStatsController.js
+++ backend/src/controllers/developmentProfilesStatsController.js
@@ -96,20 +96,5 @@ export const githubStats = async (req, res) => {
     return res
       .status(500)
       .json({ message: "Failed to fetch GitHub data", error: err });
-    // if (err.response && err.response.status === 404) {
-    //   return res
-    //     .status(404)
-    //     .json({ message: "GitHub user not found", error: err });
-    // } else if (err.code === "ECONNABORTED") {
-    //   return res
-    //     .status(504)
-    //     .json({ message: "GitHub API timeout. Try again later.", error: err });
-    // } else {
-    //   // console.error("GitHub API Error:", err.message);
-    //   return res.status(500).json({
-    //     message: "Failed to fetch GitHub data",
-    //     error: err,
-    //   });
-    // }
   }
 };
